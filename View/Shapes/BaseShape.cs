ba8f77db5e2702a8723c49b052ce86e535866787
Evgeny <kollorii@gmail.com>
2017-09-15 19:15:00 +0700

    + abstract class

---
+++ View/Shapes/BaseShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Model
+{
+    public abstract class BaseShape
+    {
+        bool draw
+        {
+            get;
+        }
+
+
+    }
+}
